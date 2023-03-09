f3f17fe3ba47652194ab0c5061c05f81cc815a3c
Marek Zelinka <mzelinka17@gmail.com>
2023-03-09 09:21:54 +0100

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { diaryRouter } from "./routes/diaries";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/ping", (_request, response) => {
